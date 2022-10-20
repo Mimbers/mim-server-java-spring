bc0262123eb61d154dbd0cdffc9081dc605595a4
joji <jojiapp@gmail.com>
2022-10-21 00:00:46 +0900

    [Feat: gradle] dependencies 설정

---
+++ src/test/java/com/jojiapp/mimserverjavaspring/MimServerJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojiapp.mimserverjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimServerJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
