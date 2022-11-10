e0bac3835ebe6441dcd74aef78e68d6f8089ceda
조지헌 <72853422+jojiapp@users.noreply.github.com>
2022-11-11 00:12:28 +0900

    [Feat: Domain] Member Entity 생성 (예제) (#8)

---
+++ mim-server-domain/common/src/main/java/com/mimbers/entity/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.mimbers.entity;
+
+
+import lombok.*;
+import org.springframework.data.annotation.*;
+
+import javax.persistence.*;
+import java.time.*;
+
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class BaseTimeEntity extends BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
---
+++ mim-server-domain/member/src/main/java/com/mimbers/entity/Address.java
@@ -0,0 +1,20 @@
+package com.mimbers.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address {
+
+    @Column(nullable = false)
+    private String zipCode;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String detailAddress;
+
+}
